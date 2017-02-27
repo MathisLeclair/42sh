9e229ccb7d1d4fdf2e502949bf3a85abe272c6a7
Mathis Leclair <mleclair@student.42.fr>
2017-02-27 13:22:51 +0100

    test

--- bquote.c
+++ bquote.c
@@ -6,7 +6,7 @@
 /*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/26 16:12:42 by mleclair          #+#    #+#             */
-/*   Updated: 2017/02/26 19:00:44 by mleclair         ###   ########.fr       */
+/*   Updated: 2017/02/27 13:10:32 by mleclair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,60 +28,57 @@ void		realoc(char *str, int size)
 {
 	char *tmp;
 
-	tmp = malloc(size * 2);
+	tmp = malloc(ft_strlen(str) * 2);
 	*tmp = 0;
 	ft_strcat(tmp, str);
 	free(str);
 	str = tmp;
 }
 
-void		bquote3(t_env *env, char *sav, int i)
+void		bquote3(t_env *env, char *sav, int i, int *fds)
 {
 	char	*str;
-	int		fd;
 
-	fd = open("/tmp/42sh_the_silence", O_RDONLY);
-	unlink("/tmp/42sh_the_silence");
 	str = malloc(INPUT_SIZE);
-	while (read(fd, str, INPUT_SIZE - 1))
-	{
-		str[INPUT_SIZE] = 0;
+	while (read(fds[0], str, INPUT_SIZE - 1))
 		realoc(str, INPUT_SIZE);
-	}
 	close(fd);
 	free(env->input);
 	env->input = malloc(ft_strlen(str));
 	env->input[0] = 0;
 	remove_nl(&str);
+	printf("sav=%s, i =%d, str= #%s\n", sav, i, str);
 	ft_strncat(env->input, sav, i);
 	ft_strcat(env->input, str);
 	ft_strcat(env->input, sav + i);
 }
 
-void		bquote2(t_env *env)
+void		bquote2(t_env *env, char *sav, int i)
 {
 	pid_t	child;
-	int		fd;
+	int		fds[2];
 
+	pipe(fds);
 	child = -1;
-	fd = -1;
-	if ((fd = open("/tmp/42sh_the_silence",
-		O_WRONLY | O_CREAT | O_TRUNC, 0600)) == -1)
-		perror("error");
 	child = fork();
 	if ((int)child == -1)
 	{
-		close(fd);
+		close(fds[1]);
+		close(fds[0]);
 		perror("error");
 	}
 	else if ((int)child == 0)
 	{
-		dup2(fd, STDOUT_FILENO);
+		dup2(fds[1], STDOUT_FILENO);
+		close(fds[0]);
 		parse(env, env->input);
 		exit(EXIT_SUCCESS);
 	}
-	close(fd);
+	dup2(fds[0], STDIN_FILENO);
+	close(fds[1]);
 	wait(NULL);
+	bquote3(env, sav, i, fds);
+
 }
 
 void	bquote(t_env *env)
@@ -90,25 +87,27 @@ void	bquote(t_env *env)
 	int		k;
 	char	*sav;
 
+	printf("test\n");
 	i = -1;
 	k = 0;
-	env->inp1 = malloc(INPUT_SIZE);
-	env->inp1[0] = 0;
+	env->inp1 = ft_strdup(env->input);
 	while (env->input[++i])
 		if (env->input[i] == '`')
 			++k;
 	if (k % 2 == 1)
-		while (!ft_strchr(env->inp1, '`'))
-			env->inp1 = ft_strjoin(env->inp1, termcaps(ft_sprintf("bquote>")));
-	else
-		env->inp1 = ft_strdup(env->input);
+	{
+		free(env->inp1);
+		env->input[0] = 0;
+		ft_putstr("unmatched `\n");
+		env->inp1 = NULL;
+		return ;
+	}
 	i = ft_strfind(env->inp1, '`');
-	k = ft_strcfind(env->inp1, '`');
+	k = ft_strfind(env->inp1 + i + 1, '`');
 	sav = ft_strdup(env->input);
 	free(env->input);
 	env->input = malloc(INPUT_SIZE);
+	env->input[0] = 0;
 	ft_strncat(env->input, env->inp1 + i + 1, k);
-	env->input[ft_strfind(env->input, '`')] = 0;
-	bquote2(env);
-	bquote3(env, sav, i);
+	bquote2(env, sav, i);
 }
