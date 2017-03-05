ad2129cc712ebb991fa3cc45bd38cf126bbd7506
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2017-03-05 17:01:31 +0100

    Scripts handle comments

--- handle_file.c
+++ handle_file.c
@@ -6,32 +6,61 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/04 17:20:45 by tgauvrit          #+#    #+#             */
-/*   Updated: 2017/03/04 18:42:22 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2017/03/05 10:23:30 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
 
-void	handle_file(int ac, char **av, t_env *env)
+static void	strip_comments(char *str)
+{
+	int i;
+	int len;
+	int quote;
+
+	if (*str == '#')
+	{
+		*str = '\0';
+		return ;
+	}
+	i = 0;
+	quote = 0;
+	len = ft_strlen(str);
+	while (++i < len)
+	{
+		if ((str[i] == '\'' || str[i] == '\"') && (quote % (str[i] % 6)) == 0)
+			quote ^= str[i] % 6;
+		if (str[i] == '#' && quote == 0)
+		{
+			if (str[i - 1] != '\\')
+				str[i] = '\0';
+			else
+			{
+				ft_strcpy(str + i - 1, str + i);
+				--i;
+			}
+		}
+	}
+}
+
+void		handle_file(int ac, char **av, t_env *env)
 {
 	int		fd;
 	char	*input;
 
 	if (ac < 2)
 		return ;
-	// is file?
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 	{
 		ft_printf("shell: %s: no such file\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	//argv
 	handle_argv(ac, av, env);
-	//pass file to input
 	input = NULL;
 	while (get_next_line(fd, &input))
 	{
+		strip_comments(input);
 		if (ft_read(env, input) != 0)
 		{
 			env_free(env);
