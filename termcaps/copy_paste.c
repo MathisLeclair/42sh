c7add380c5d46c64abb26d86075e45a9896570c7
Antonin RIDOLFI <aridolfi@student.42.fr>
2017-03-28 14:49:56 +0200

    hotfix

--- termcaps/copy_paste.c
+++ termcaps/copy_paste.c
@@ -6,7 +6,7 @@
 /*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 13:33:41 by mleclair          #+#    #+#             */
-/*   Updated: 2017/03/28 13:53:02 by mleclair         ###   ########.fr       */
+/*   Updated: 2017/03/28 13:56:18 by aridolfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,12 @@ void	paste(t_var *var)
 	i = ft_strlen(var->cpy);
 	while (i--)
 		add_car(var, 1, var->cpy[i]);
-	i = var->i;
 	while (var->i > 0)
 		left_arrow(var);
 	ft_putstr(tgetstr("cd", NULL));
 	ft_putstr(var->ret);
 	var->i = ft_strlen(var->ret);
 	var->lenligne = ft_strlen(var->ret) + var->lenprompt;
-	while (var->i > i)
-		left_arrow(var);
 	var->selend = -1;
 	var->selstart = -1;
 }
