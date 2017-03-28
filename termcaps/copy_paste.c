c5186dfad744a19660b798a3378880f6f71eadc2
Mathis Leclair <mleclair@student.42.fr>
2017-03-28 13:53:36 +0200

    fix paste

--- termcaps/copy_paste.c
+++ termcaps/copy_paste.c
@@ -6,24 +6,44 @@
 /*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/18 13:33:41 by mleclair          #+#    #+#             */
-/*   Updated: 2017/03/18 13:34:48 by mleclair         ###   ########.fr       */
+/*   Updated: 2017/03/28 13:53:02 by mleclair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../chell.h"
 
+// void	paste(t_var *var)
+// {
+// 	int i;
+
+// 	ft_putstr(var->cpy);
+// 	i = ft_strlen(var->cpy);
+// 	while (i--)
+// 		add_car(var, 1, var->cpy[i]);
+// 	var->i += ft_strlen(var->cpy);
+// 	i = var->i;
+// 	ft_putstr(tgetstr("cd", NULL));
+// 	ft_putstr(var->ret + var->i);
+// 	var->i = ft_strlen(var->ret);
+// 	var->lenligne = ft_strlen(var->ret) + var->lenprompt;
+// 	while (var->i > i)
+// 		left_arrow(var);
+// 	var->selend = -1;
+// 	var->selstart = -1;
+// }
+
 void	paste(t_var *var)
 {
 	int i;
 
-	ft_putstr(var->cpy);
 	i = ft_strlen(var->cpy);
 	while (i--)
 		add_car(var, 1, var->cpy[i]);
-	var->i += ft_strlen(var->cpy);
 	i = var->i;
+	while (var->i > 0)
+		left_arrow(var);
 	ft_putstr(tgetstr("cd", NULL));
-	ft_putstr(var->ret + var->i);
+	ft_putstr(var->ret);
 	var->i = ft_strlen(var->ret);
 	var->lenligne = ft_strlen(var->ret) + var->lenprompt;
 	while (var->i > i)
