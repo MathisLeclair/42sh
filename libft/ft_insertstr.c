e3df79dc1cc345cfae6cf7ffb64be8a4d7d6527a
Antonin RIDOLFI <aridolfi@student.42.fr>
2017-04-01 15:31:02 +0200

    hotfix

--- libft/ft_insertstr.c
+++ libft/ft_insertstr.c
@@ -6,7 +6,7 @@
 /*   By: aridolfi <aridolfi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 14:08:58 by aridolfi          #+#    #+#             */
-/*   Updated: 2017/04/01 15:28:09 by aridolfi         ###   ########.fr       */
+/*   Updated: 2017/04/01 15:30:59 by aridolfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 void	ft_insertstr(char **src, char *str, size_t n)
 {
 	char	*new;
+	char	*tmp;
 	size_t	i;
 
 	i = 0;
 	new = ft_strnew(ft_strlen(*src) + ft_strlen(str));
-	ft_strcat(new, ft_strsub(*src, 0, n + 1));
+	tmp = ft_strsub(*src, 0, n + 1);
+	ft_strcat(new, tmp);
 	ft_strcat(new, str);
 	ft_strcat(new, *src + (n + 1));
+	free(tmp);
 	free(*src);
 	*src = new;
 }
