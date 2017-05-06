81c048944f1cad324194e35e70b5961321af6c8a
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2017-05-06 14:04:20 +0200

    conditions (hide debug)

--- condition.c
+++ condition.c
@@ -47,23 +47,23 @@
 // 	char	*str;
 // 	char	**tmp;
 
-// 	ft_putendl_fd("join_split 1", env()->fdout);
+	// ft_putendl_fd("join_split 1", env()->fdout);
 // 	size = 0;
 // 	tmp = split - 1;
 // 	while (*(++tmp) != NULL)
 // 		size += ft_strlen(*tmp) + ft_strlen(glue);
-// 	ft_putendl_fd("join_split 2", env()->fdout);
+	// ft_putendl_fd("join_split 2", env()->fdout);
 // 	str = palloc(sizeof(char) * (size + 1));
 // 	str[0] = '\0';
 // 	tmp = split - 1;
-// 	ft_putendl_fd("join_split 3", env()->fdout);
+	// ft_putendl_fd("join_split 3", env()->fdout);
 // 	while (*(++tmp) != NULL)
 // 	{
 // 		ft_strcat(str, *tmp);
 // 		ft_strcat(str, glue);
 // 	}
 // 	str[size - 1] = '\0';
-// 	ft_putendl_fd("join_split 4", env()->fdout);
+	// ft_putendl_fd("join_split 4", env()->fdout);
 // 	return (str);
 // }
 
@@ -86,22 +86,22 @@ void	destroy_condition(t_cond *cond)
 	t_cond	*block;
 	t_cond	*tmp;
 
-	ft_putendl_fd("destroy_condition 1", env()->fdout);
+	// ft_putendl_fd("destroy_condition 1", env()->fdout);
 	block = cond->block;
 	while (block != NULL)
 	{
-		ft_putendl_fd("destroy_condition 2", env()->fdout);
+		// ft_putendl_fd("destroy_condition 2", env()->fdout);
 		if (block->type == IS_LINE)
 		{
-			ft_putendl_fd("destroy_condition 3.1", env()->fdout);
+			// ft_putendl_fd("destroy_condition 3.1", env()->fdout);
 			free(block->content);
-			ft_putendl_fd("destroy_condition 4.1", env()->fdout);
+			// ft_putendl_fd("destroy_condition 4.1", env()->fdout);
 		}
 		else
 		{
-			ft_putendl_fd("destroy_condition 3.2", env()->fdout);
+			// ft_putendl_fd("destroy_condition 3.2", env()->fdout);
 			destroy_condition(block->content);
-			ft_putendl_fd("destroy_condition 4.2", env()->fdout);
+			// ft_putendl_fd("destroy_condition 4.2", env()->fdout);
 		}
 		tmp = block;
 		block = block->block;
@@ -113,7 +113,7 @@ void	destroy_condition(t_cond *cond)
 
 int		do_if_condition(t_env *env, char *input)
 {
-	ft_putendl_fd("do_if_condition 1", env->fdout);
+	// ft_putendl_fd("do_if_condition 1", env->fdout);
 	if (!ft_strncmp(input, "if ", 3))
 		new_condition(COND_IF, env, input);
 	else if (!ft_strncmp(input, "while ", 6))
@@ -122,10 +122,10 @@ int		do_if_condition(t_env *env, char *input)
 		new_condition(COND_FOR, env, input);
 	else
 	{
-		ft_putendl_fd("do_if_condition 2.1", env->fdout);
+		// ft_putendl_fd("do_if_condition 2.1", env->fdout);
 		return (0);
 	}
-	ft_putendl_fd("do_if_condition 2.2", env->fdout);
+	// ft_putendl_fd("do_if_condition 2.2", env->fdout);
 	return (1);
 }
 
@@ -144,25 +144,25 @@ void	add_to_block(t_cond *cond, t_env *env, char *input)
 {
 	t_cond	*block;
 
-	ft_putendl_fd("add_to_block 1", env->fdout);
+	// ft_putendl_fd("add_to_block 1", env->fdout);
 	block = cond;
-	ft_putendl_fd("add_to_block 2", env->fdout);
+	// ft_putendl_fd("add_to_block 2", env->fdout);
 	while (block->block != NULL)
 		block = block->block;
-	ft_putendl_fd("add_to_block 3", env->fdout);
+	// ft_putendl_fd("add_to_block 3", env->fdout);
 	if (do_if_condition(env, input))
 	{
-		ft_putendl_fd("add_to_block 4.1", env->fdout);
+		// ft_putendl_fd("add_to_block 4.1", env->fdout);
 		block->block = new_block_part(IS_COND, env->cond);
-		ft_putendl_fd("add_to_block 5.1", env->fdout);
+		// ft_putendl_fd("add_to_block 5.1", env->fdout);
 	}
 	else
 	{
-		ft_putendl_fd("add_to_block 4.2", env->fdout);
+		// ft_putendl_fd("add_to_block 4.2", env->fdout);
 		block->block = new_block_part(IS_LINE, ft_strdup(input));
-		ft_putendl_fd("add_to_block 5.2", env->fdout);
+		// ft_putendl_fd("add_to_block 5.2", env->fdout);
 	}
-	ft_putendl_fd("add_to_block 6", env->fdout);
+	// ft_putendl_fd("add_to_block 6", env->fdout);
 }
 
 void	exec_condition(t_env *env, t_cond *cond)
@@ -194,26 +194,26 @@ void	exec_condition(t_env *env, t_cond *cond)
 					return ;
 			}
 		}
-		ft_putendl_fd("exec_condition 1", env->fdout);
+		// ft_putendl_fd("exec_condition 1", env->fdout);
 		block = cond->block;
 		while (block != NULL)
 		{
-			ft_putendl_fd("exec_condition 2", env->fdout);
+			// ft_putendl_fd("exec_condition 2", env->fdout);
 			if (block->type == IS_LINE)
 			{
-				ft_putendl_fd("exec_condition 3.1", env->fdout);
+				// ft_putendl_fd("exec_condition 3.1", env->fdout);
 				parse(env, (char**)(&(block->content)), 1);
-				ft_putendl_fd("exec_condition 4.1", env->fdout);
+				// ft_putendl_fd("exec_condition 4.1", env->fdout);
 			}
 			else
 			{
-				ft_putendl_fd("exec_condition 3.2", env->fdout);
+				// ft_putendl_fd("exec_condition 3.2", env->fdout);
 				exec_condition(env, block->content);
-				ft_putendl_fd("exec_condition 4.2", env->fdout);
+				// ft_putendl_fd("exec_condition 4.2", env->fdout);
 			}
 			block = block->block;
 		}
-		ft_putendl_fd("exec_condition 5", env->fdout);
+		// ft_putendl_fd("exec_condition 5", env->fdout);
 		if (cond->type == COND_IF)
 			return ;
 	}
@@ -224,20 +224,20 @@ void	handle_condition(t_env *env, char *input)
 	t_cond	*cond;
 
 	cond = env->cond;
-	ft_putendl_fd("handle_condition 1", env->fdout);
+	// ft_putendl_fd("handle_condition 1", env->fdout);
 	if (cond->block == NULL && ft_strncmp(input, "do ", 3) != 0) //   NO DO BLOCK PRESENT
 		cond->has_block = 0;
 	else if (cond->block == NULL && ft_strncmp(input, "do ", 3) == 0) // DO BLOCK PRESENT
 		input = input + 3; // IGNORE 'DO' OF COMMAND, IT'S DONE ITS JOB
-	ft_putendl_fd("handle_condition 2", env->fdout);
+	// ft_putendl_fd("handle_condition 2", env->fdout);
 	if (ft_strcmp(input, "done") == 0)
 		cond->has_block = 0;
 	else
 		add_to_block(cond, env, input);
-	ft_putendl_fd("handle_condition 3", env->fdout);
+	// ft_putendl_fd("handle_condition 3", env->fdout);
 	while (env->cond->has_block == 0 && env->cond->parent != NULL)
 		env->cond = env->cond->parent;
-	ft_putendl_fd("handle_condition 4", env->fdout);
+	// ft_putendl_fd("handle_condition 4", env->fdout);
 	if (env->cond->has_block == 0)
 	{
 		cond = env->cond;
@@ -245,5 +245,5 @@ void	handle_condition(t_env *env, char *input)
 		exec_condition(env, cond);
 		destroy_condition(cond);
 	}
-	ft_putendl_fd("handle_condition 5", env->fdout);
+	// ft_putendl_fd("handle_condition 5", env->fdout);
 }
