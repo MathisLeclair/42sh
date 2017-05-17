875e8dd7a5924901715d200ab4bc4fb1953bb813
Mathis Leclair <mleclair@student.42.fr>
2017-05-17 16:23:00 +0200

    norm

--- builtin_alias.c
+++ builtin_alias.c
@@ -6,7 +6,7 @@
 /*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 15:52:16 by mleclair          #+#    #+#             */
-/*   Updated: 2017/05/17 16:15:20 by mleclair         ###   ########.fr       */
+/*   Updated: 2017/05/17 16:22:51 by mleclair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ void	alias(t_env *env, char **split)
 	while (split[++i])
 	{
 		u = 0;
-		if (find_param(env->alias, split[i]) == -1 && ft_strfind(split[i], '=') != 0)
+		if (find_param(env->alias, split[i]) == -1 &&
+			ft_strfind(split[i], '=') != 0)
 			add_str_to_dstr(&env->alias, split[i]);
 		else
 		{
