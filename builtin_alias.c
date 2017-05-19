e38f896ddfc4b7b29134744ac03a03ac5880875f
Baptiste FROCHOT <bfrochot@student.42.fr>
2017-05-19 16:58:12 +0200

    erreuralias

--- builtin_alias.c
+++ builtin_alias.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_alias.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mleclair <mleclair@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bfrochot <bfrochot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 15:52:16 by mleclair          #+#    #+#             */
-/*   Updated: 2017/05/17 16:22:51 by mleclair         ###   ########.fr       */
+/*   Updated: 2017/05/19 16:57:35 by bfrochot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	alias(t_env *env, char **split)
 	{
 		u = 0;
 		if (find_param(env->alias, split[i]) == -1 &&
-			ft_strfind(split[i], '=') != 0)
+			ft_strfind(split[i], '=') != -1)
 			add_str_to_dstr(&env->alias, split[i]);
 		else
 		{
-			if ((u = ft_strfind(split[i], '=')) == 0)
+			if ((u = ft_strfind(split[i], '=')) == -1)
 				return (error(-4, split[i], NULL));
 			u = find_param(env->alias, split[i]);
 			free(env->alias[u]);
